9433d7619b802f26bf98a8277ff0fd31feff9ea1
Aleksey Tomilov <suphuilo@gmail.com>
2017-02-07 21:09:05 +0800

    сложение

---
+++ calc.java
@@ -0,0 +1,9 @@
+public class calc {
+    public static void main(String[] args){
+        System.out.println(func_add(10.8,5));
+    }
+
+    private static double func_add(double a, double b){
+        return a+b;
+    }
+}
