a8868ebc957c1d94d9e415b8d40c159bf2922245
Aleksey Tomilov <suphuilo@gmail.com>
2017-02-07 21:13:11 +0800

    вычитание issue #1

--- calc.java
+++ calc.java
@@ -1,9 +1,15 @@
 public class calc {
     public static void main(String[] args){
         System.out.println(func_add(10.8,5));
+	System.out.println(func_sub(10.8,5));
     }
 
     private static double func_add(double a, double b){
         return a+b;
     }
+
+    private static double func_sub(double a, double b){
+        return a-b;
+    }
+
 }
