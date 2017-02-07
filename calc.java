eb766ceb8519b1ee094807e408f4719d6dd8c1de
Aleksey Tomilov <suphuilo@gmail.com>
2017-02-07 21:15:44 +0800

    умножение  issue #2

--- calc.java
+++ calc.java
@@ -2,6 +2,7 @@ public class calc {
     public static void main(String[] args){
         System.out.println(func_add(10.8,5));
 	System.out.println(func_sub(10.8,5));
+	System.out.println(func_mul(10.8,5));
     }
 
     private static double func_add(double a, double b){
@@ -10,6 +11,9 @@ public class calc {
 
     private static double func_sub(double a, double b){
         return a-b;
+    }
+    private static double func_mul(double a, double b){
+        return 2*a;
     }
 
 }
